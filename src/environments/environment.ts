9dbc505ca716bacbd9eed15a45d58495e2966f9f
Brunno Khouri <brunno.k.d.borges@gmail.com>
2022-11-23 13:39:44 -0300

    add-FirebaseSettings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD_vCMeYS94JQFa7dHAXk921VA3Xzoz2T8",
+    authDomain: "projectfirebase-7ffac.firebaseapp.com",
+    databaseURL: "https://projectfirebase-7ffac-default-rtdb.firebaseio.com",
+    projectId: "projectfirebase-7ffac",
+    storageBucket: "projectfirebase-7ffac.appspot.com",
+    messagingSenderId: "114411177015",
+    appId: "1:114411177015:web:33e62ab9e1c8c8bdf7abb2",
+    measurementId: "G-YVEYQTT53M"
+  }
 };
 
 /*
